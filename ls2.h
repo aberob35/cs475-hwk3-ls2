fb6dbe2f9b5ca3ee8790833e7a7cef4d7f908db6
aberob35 <arobin@pugetsound.edu>
2023-02-06 15:00:57 -0800

    Update ls2.h

--- ls2.h
+++ ls2.h
@@ -2,11 +2,15 @@
 #define LS2_H_INCLUDED
 
 #include "stack.h"
+#include <unistd.h>
+#include <sys/stat.h>
 
 #define TRUE 1
 #define FALSE 0
 #define INDENT "    "
 
 // TODO: Function declarations here for ls2
+void recDirectoryOpenerM1(char *dName);
+void recDirectoryOpenerM2(char *dName, char *fName);
 
 #endif
