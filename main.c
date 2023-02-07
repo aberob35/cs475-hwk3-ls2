6a3af3e1fd7944006a5a1ec6caee478b222117b9
aberob35 <arobin@pugetsound.edu>
2023-02-06 19:47:06 -0800

    Update main.c

--- main.c
+++ main.c
@@ -10,24 +10,17 @@
  * Usage: ls2 <path> [exact-match-pattern]
  */
 int main(int argc, char *argv[]) {
-	//int length = sizeof(&argv)/sizeof(argv[0]);
-	//printf("%d ",length);
 	printf("Contents inside of directory \n");
-	//recDirectoryOpenerM2(argv[1],argv[2]);
-
 	if(argc == 2){
 		recDirectoryOpenerM1(argv[1]);
-	}else if(argc > 2){
+	}else if(argc == 3){
 		recDirectoryOpenerM2(argv[1], argv[2]);
+	} else if(argc > 3 ){
+		printf("Too many things to search for");
+	} else if(argc <= 1){
+		printf("Too little things inputted for program to run");
 	}
-	// I didnt use stack but it wasnt a requirment
-	// I know how to use them I would just push 
-	//everything onto the stack where I am priniting
-	// in my code and then print out the stack to have the 
-	//stack be utilized in order to print out the directory and file 
-	//to user. I mode 2 I would have to peek at data fro conditional 
-	//and pop off for print
-
+	
 	// //stack stores the lines to print out
 	// stack_t *s = initstack();
 	// push(s, "Hello1");
